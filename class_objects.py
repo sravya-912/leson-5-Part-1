040adef99aac2d000e0ff493f43a7561284ad06d
sravya-912 <vsravya912@gmail.com>
2024-11-20 19:26:57 +0000

    Add files via upload

---
+++ class_objects.py
@@ -0,0 +1,17 @@
+class Student:
+
+    attr1="Sravya"
+    attr2="12"
+    attr3="Badminton"
+
+
+    def fun(self):
+        print("My name is", self.attr1)
+        print("My age is", self.attr2)
+        print("My hobby is", self.attr3)
+
+
+Sravya= Student()
+
+print (Sravya.attr1)
+Sravya.fun()
